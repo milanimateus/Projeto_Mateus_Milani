7a74eae70303be3d4d05d4299a7730e7a931df9a
milanimateus <mateusmilanir26@gmail.com>
2024-10-28 09:37:25 -0300

    Criando a classe D e seus métodos

---
+++ ex7/src/D.java
@@ -0,0 +1,25 @@
+public class D {
+    private int d1;
+    private float d2;
+
+    public D(int d1, float d2){
+        this.d1 = d1;
+        this.d2 = d2;
+    }
+
+    public int getD1(){
+        return d1;
+    }
+
+    public float getD2(){
+        return d2;
+    }
+
+    public void md1(){
+        System.out.println("Nome do Método: md1()");
+    }
+
+    public void md2(){
+        System.out.println("Nome do Método: md2()");
+    }
+}
